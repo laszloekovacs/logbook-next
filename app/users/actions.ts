4fd333abcd801fe4196720c729dfbaa136bc089e
laszloekovacs <laszloekovacs@gmail.com>
2025-08-26 19:50:48 +0200

    add zod validator

--- app/users/actions.ts
+++ app/users/actions.ts
@@ -1,10 +1,20 @@
+import z from "zod"
+
+
+
 export async function createUser(formData: FormData) {
     "use server"
 
-    const name = formData.get('name')
+    const createUserSchema = z.object({
+        name: z.string()
+    })
 
-    console.log("recieved: " + name )
+    const validatedFields = createUserSchema.safeParse({
+        name: formData.get("name")
+    })
 
+    console.log(validatedFields)
+    
     // revalidate if users are displayed on same page
     // reavalidatePath("/users")
 }
